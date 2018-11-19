d723a2a2c70b595ff00f164e53af9a5bc6c0d885
kota718 <kota.yt@gmail.com>
2018-11-19 17:02:39 +0900

    fishファイル更新

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,44 @@
-set -x EDITOR vim
-set -x TERM xterm-256color
-set -x PATH /usr/local/bin $PATH
-set -gx VIM /usr/local/bin/nvim
-set -gx VIMRUNTIME /usr/local/Cellar/neovim/0.1.7/share/nvim/runtime
+set --export EDITOR vm
+set --export MANPATH /usr/share/man $MANPATH
+
+# vi mode
+set -U fish_key_bindings fish_vi_key_bindings
+
+# add ~/.local/bin
+set --export PATH ~/.local/bin $PATH
+
+# linuxbrew
+set --export PATH /home/linuxbrew/.linuxbrew/bin $PATH
+set --export PATH /home/linuxbrew/.linuxbrew/sbin $PATH
+set --export MANPATH /home/linuxbrew/.linuxbrew/share/man $MANPATH
+set --export INFOPATH /home/linuxbrew/.linuxbrew/share/info $INFOPATH
 
 # anyenv
-set -x PATH $PATH ~/.anyenv/bin
-
+set --export PATH ~/.anyenv/bin $PATH
 for dir in (ls ~/.anyenv/envs)
-  set -x  PATH $PATH ~/.anyenv/envs/$dir/bin
-  set -x PATH $PATH ~/.anyenv/envs/$dir/shims
+set --export  PATH ~/.anyenv/envs/$dir/bin $PATH
+set --export PATH ~/.anyenv/envs/$dir/shims $PATH
 end
 
-. $HOME/.anyenv/completions/anyenv.fish
-anyenv init - > /dev/null
+# FZF
+set --export FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+set --export FZF_DEFAULT_OPTS '--height 40 --reverse --inline-info'
+set --export FZF_FIND_FILE_COMMAND $FZF_DEFAULT_COMMAND
 
-alias vim 'nvim'
+# alias
+alias pbcopy 'xsel --clipboard --input'
+alias gco 'git branch | fzf | xargs git checkout'
 
-for file in ~/.config/fish/conf.d/*.fish
-  source $file
+#function fish_user_key_bindings
+#  bind \cl clear
+#  bind -M default \$ end-of-line accept-autosuggestion
+#end
+#fish_vi_key_bindings
+#set -g fish_escape_delay_ms 10
+
+# 端末間でヒストリーを共有
+function history-merge --on-event fish_preexec
+  history --save
+  history --merge
 end
 
-set HISTCONTROL ignoreboth
-set HISTIGNORE  pwd:ls:history
