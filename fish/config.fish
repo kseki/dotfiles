e5455f914c01501e64ea1253b299973a18f06577
kota718 <kota.yt@gmail.com>
2018-11-20 14:24:57 +0900

    anyenvの設定を外だし

--- fish/config.fish
+++ fish/config.fish
@@ -1,25 +1,21 @@
-set -x EDITOR vim
-set -x TERM xterm-256color
+set --export EDITOR vim
+#set -x TERM xterm-256color
 set -x PATH /usr/local/bin $PATH
-set -gx VIM /usr/local/bin/nvim
-set -gx VIMRUNTIME /usr/local/Cellar/neovim/0.1.7/share/nvim/runtime
+#set -gx VIM /usr/local/bin/nvim
+#set -gx VIMRUNTIME /usr/local/Cellar/neovim/0.1.7/share/nvim/runtime
 
 # anyenv
-set -x PATH $PATH ~/.anyenv/bin
+set --export PATH $PATH ~/.anyenv/bin
 
 for dir in (ls ~/.anyenv/envs)
-  set -x  PATH $PATH ~/.anyenv/envs/$dir/bin
-  set -x PATH $PATH ~/.anyenv/envs/$dir/shims
-end
-
-. $HOME/.anyenv/completions/anyenv.fish
-anyenv init - > /dev/null
-
-alias vim 'nvim'
-
-for file in ~/.config/fish/conf.d/*.fish
-  source $file
+  set --export PATH ~/.anyenv/envs/$dir/bin $PATH
+  set --export PATH ~/.anyenv/envs/$dir/shims $PATH
 end
 
 set HISTCONTROL ignoreboth
 set HISTIGNORE  pwd:ls:history
+
+set --export FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+set --export FZF_DEFAULT_OPTS '--height 40 --reverse --inline-info'
+
+. ~/.config/fish/anyenv.fish
