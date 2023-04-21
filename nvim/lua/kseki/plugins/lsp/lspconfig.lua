e68204a3b6060bca9af6d39de0cc39553e4ae9eb
Kota Seki <kota.yt@gmail.com>
2023-04-22 02:32:25 +0900

    プラグインの非同期読み込み

--- nvim/lua/kseki/plugins/lsp/lspconfig.lua
+++ nvim/lua/kseki/plugins/lsp/lspconfig.lua
@@ -1,48 +1,53 @@
 local lspconfig_status, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status then
-  return
+	return
 end
 
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
-  return
+	return
 end
 
 local keymap = vim.keymap
 
 -- Enable keybindings for available lsp server
 local on_attach = function(client, bufnr)
-  local opts = { noremap = true, silent = true, buffer = bufnr }
+	local opts = { noremap = true, silent = true, buffer = bufnr }
 
-  -- Set keybinds
-  keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)
-  keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
-  keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
-  keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
-  keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-  keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
-  keymap.set("n", "d[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
-  keymap.set("n", "d]", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
-  keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-  keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
+	-- Set keybinds
+	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)
+	keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+	keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+	keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+	keymap.set("n", "d[", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
+	keymap.set("n", "d]", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
+	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+	keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<CR>", opts)
 end
 
 -- Used to enable autocompletion
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 lspconfig["html"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 lspconfig["cssls"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
 
 lspconfig["solargraph"].setup({
-  capabilities = capabilities,
-  on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
+})
+
+lspconfig["rubocop"].setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
 })
--- nvim/lua/kseki/plugins/telescope.lua
+++ nvim/lua/kseki/plugins/telescope.lua
@@ -26,6 +26,14 @@ telescope.setup({
 			},
 		},
 	},
+	extensions = {
+		fzf = {
+			fuzzy = true, -- false will only do exact matching
+			override_generic_sorter = true, -- override the generic sorter
+			override_file_sorter = true, -- override the file sorter
+			case_mode = "smart_case",
+		},
+	},
 })
 
 telescope.load_extension("fzf")
