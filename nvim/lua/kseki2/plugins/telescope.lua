54e1e4f8f03d300b7d2c71a55921f779d031980f
Kota Seki <kota.yt@gmail.com>
2025-01-18 12:52:56 +0900

    feat(telescope): Gitステータスを表示するマッピングを追加
    Gitのステータスを簡単に確認できるようにするため、<leader>fsの
    マッピングを追加しました。

--- nvim/lua/kseki2/plugins/telescope.lua
+++ nvim/lua/kseki2/plugins/telescope.lua
@@ -7,5 +7,6 @@ return {
 		{ "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = "Grep files", mode = "n" },
 		{ "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", desc = "Find buffer", mode = "n" },
 		{ "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", desc = "Find help", mode = "n" },
+		{ "<leader>fs", "<cmd>lua require('telescope.builtin').git_status()<CR>", desc = "Git status", mode = "n" },
 	},
 }
