1f8e67c16e0821fc29855b8135169e886ee6402b
Kota Seki <kota.yt@gmail.com>
2025-01-18 11:31:59 +0900

    feat: Telescopeのキーマッピングを変更
    Telescopeのキーマッピングをより直感的にするために変更しました。

--- nvim/lua/kseki2/plugins/telescope.lua
+++ nvim/lua/kseki2/plugins/telescope.lua
@@ -3,9 +3,9 @@ return {
 	branch = "0.1.x",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
-		{ "<leader>f", "<cmd>lua require('telescope.builtin').find_files()<CR>", desc = "Find file", mode = "n" },
-		{ "<leader>g", "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = "Grep files", mode = "n" },
-		{ "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>", desc = "Find buffer", mode = "n" },
-		{ "<leader>h", "<cmd>lua require('telescope.builtin').help_tags()<CR>", desc = "Find help", mode = "n" },
+		{ "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", desc = "Find file", mode = "n" },
+		{ "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = "Grep files", mode = "n" },
+		{ "<leader>fb", "<cmd>lua require('telescope.builtin').buffers()<CR>", desc = "Find buffer", mode = "n" },
+		{ "<leader>fh", "<cmd>lua require('telescope.builtin').help_tags()<CR>", desc = "Find help", mode = "n" },
 	},
 }
