a8b0547c868965a1664a2fddc3f13af63c7d7072
Kota Seki <kota.yt@gmail.com>
2025-01-18 10:41:32 +0900

    feat(telescope): キーマッピングに説明とモードを追加
    ユーザーがキーマッピングの目的を理解しやすくするために、
    説明（desc）とモード（mode）を追加しました。

--- nvim/lua/kseki2/plugins/telescope.lua
+++ nvim/lua/kseki2/plugins/telescope.lua
@@ -2,9 +2,9 @@ return {
 	'nvim-telescope/telescope.nvim', branch = '0.1.x',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	keys = {
-		{ "<leader>f", "<cmd>lua require('telescope.builtin').find_files()<CR>" },
-		{ "<leader>g", "<cmd>lua require('telescope.builtin').live_grep()<CR>" },
-		{ "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>" },
-		{ "<leader>h", "<cmd>lua require('telescope.builtin').help_tags()<CR>" },
+		{ "<leader>f", "<cmd>lua require('telescope.builtin').find_files()<CR>", desc = "Find file", mode = "n" },
+		{ "<leader>g", "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = "Grep files", mode = "n" },
+		{ "<leader>b", "<cmd>lua require('telescope.builtin').buffers()<CR>", desc = "Find buffer", mode = "n" },
+		{ "<leader>h", "<cmd>lua require('telescope.builtin').help_tags()<CR>", desc = "Find help", mode = "n" },
 	}
 }
