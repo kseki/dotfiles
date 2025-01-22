736bab1008d0634bff9c456d4f9c153bb192596b
Kota Seki <kota.yt@gmail.com>
2025-01-22 21:57:39 +0900

    feat: lspsagaをインストールし、LSPの機能使ってキーマップを追加
    lspsagaを導入することで、LSPの機能を強化し、より便利なキーマップを
    追加しました。

--- nvim/lua/kseki2/libs/_set_lsp.lua
+++ nvim/lua/kseki2/libs/_set_lsp.lua
@@ -11,4 +11,26 @@ function M.conf_lsp(name)
 	end
 end
 
+function M.on_attach(_, bufnr)
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+
+	vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.formatting()<cr>", bufopts)
+	vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", bufopts)
+	vim.keymap.set("n", "ca", "<cmd>Lspsaga code_action<cr>", bufopts)
+	vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<cr>", bufopts)
+	vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<cr>", bufopts)
+	vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", bufopts)
+	vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", bufopts)
+	vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", bufopts)
+	vim.keymap.set("n", "<leader>o", "<cmd>LSoutlineToggle<cr>", bufopts)
+
+	-- for Diagnostic
+	vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<cr>", bufopts)
+	vim.keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<cr>", bufopts)
+	vim.keymap.set("n", "d[", "<cmd>Lspsaga diagnostic_jump_prev<cr>", bufopts)
+	vim.keymap.set("n", "d]", "<cmd>Lspsaga diagnostic_jump_next<cr>", bufopts)
+end
+
 return M
