c80b8a4b077939b386bd90e2564480193122e763
Kota Seki <kota.yt@gmail.com>
2025-01-29 13:32:51 +0900

    feat(nvim-cmp): 特定のファイルタイプで補完を無効化
    yamlやmarkdownなどの特定のファイルタイプで補完を無効化するための設定を追加。

--- nvim/lua/kseki2/plugins/nvim-cmp.lua
+++ nvim/lua/kseki2/plugins/nvim-cmp.lua
@@ -15,6 +15,17 @@ return {
 				opts = {
 					suggestion = { enabled = false },
 					panel = { enabled = false },
+					filetypes = {
+						yaml = true,
+						markdown = true,
+						help = false,
+						gitcommit = false,
+						gitrebase = false,
+						hgcommit = false,
+						svn = false,
+						cvs = false,
+						["."] = false,
+					},
 				},
 			},
 		},
