025b7807e04e831e55648bc494cf437117016b45
Kota Seki <kota.yt@gmail.com>
2025-01-29 13:24:24 +0900

    feat: Add telescop-fzf-native

--- nvim/lua/kseki2/plugins/telescope.lua
+++ nvim/lua/kseki2/plugins/telescope.lua
@@ -1,7 +1,24 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "0.1.x",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				fzf = {
+					fuzzy = true,
+					override_generic_sorter = true,
+					override_file_sorter = true,
+					case_mode = "smart_case",
+				},
+			},
+		})
+
+		require("telescope").load_extension("fzf")
+	end,
 	keys = {
 		{ "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", desc = "Find file", mode = "n" },
 		{ "<leader>fg", "<cmd>lua require('telescope.builtin').live_grep()<CR>", desc = "Grep files", mode = "n" },
